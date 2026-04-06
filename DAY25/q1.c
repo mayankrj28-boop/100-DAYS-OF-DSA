69d4eb06901766168644b6281805f5ccd101b1d0
mayankrj28-boop <mayankrj28@gmail.com>
2026-04-06 12:34:56 +0530

    Create q1.c

---
+++ DAY25/q1.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define structure for linked list node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+int main() {
+    int n, value, key;
+    struct Node *head = NULL, *temp = NULL, *newNode;
+    int count = 0;
+
+    // Input number of nodes
+    scanf("%d", &n);
+
+    // Create linked list
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        newNode = createNode(value);
+
+        if(head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    // Input key
+    scanf("%d", &key);
+
+    // Count occurrences
+    temp = head;
+    while(temp != NULL) {
+        if(temp->data == key) {
+            count++;
+        }
+        temp = temp->next;
+    }
+
+    // Output result
+    printf("%d\n", count);
+
+    return 0;
+}
