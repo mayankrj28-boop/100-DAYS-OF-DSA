aebdcfb0ee7de1b44e304d3f23dc924a30525718
mayankrj28-boop <mayankrj28@gmail.com>
2026-04-07 23:47:21 +0530

    Create q1.c

---
+++ DAY28/q1.c
@@ -0,0 +1,49 @@
+/* structure of Node
+struct Node {
+    int data;
+    Node* next;
+};
+*/
+
+class Solution {
+public:
+    bool removeLoop(Node* head) {
+        
+        if (head == NULL || head->next == NULL)
+            return false;
+
+        Node *slow = head, *fast = head;
+
+        // Step 1: Detect loop
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast)
+                break;
+        }
+
+        // No loop found
+        if (slow != fast)
+            return false;
+
+        // Step 2: Find start of loop
+        slow = head;
+
+        if (slow == fast) {
+            while (fast->next != slow)
+                fast = fast->next;
+        } 
+        else {
+            while (slow->next != fast->next) {
+                slow = slow->next;
+                fast = fast->next;
+            }
+        }
+
+        // Step 3: Remove loop
+        fast->next = NULL;
+
+        return true;
+    }
+};
