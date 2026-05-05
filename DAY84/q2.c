eeab026c7aa91556374c16d5b7826586f151cc0f
mayankrj28-boop <mayankrj28@gmail.com>
2026-05-05 22:46:36 +0530

    Create q2.c

---
+++ DAY84/q2.c
@@ -0,0 +1,17 @@
+int findPeakElement(int* nums, int numsSize) {
+    int left = 0, right = numsSize - 1;
+
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] > nums[mid + 1]) {
+            // Peak is on the left side (including mid)
+            right = mid;
+        } else {
+            // Peak is on the right side
+            left = mid + 1;
+        }
+    }
+
+    return left;  // or right (both are same here)
+}
